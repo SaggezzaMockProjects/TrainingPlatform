15dcd7e14dd5de57f48c641d13ca1f786932a94b
Jordan.Chuipek <Jordan.Chuipek>
2016-06-15 09:11:55 -0500

    Yeoman Code Generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trainingPlatformApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trainingPlatformApp
+ */
+angular.module('trainingPlatformApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
